6152b072f5eeee4cf85df63926e2476c3670c0e6
Yosoyret <randyenyer1@gmail.com>
2026-03-05 18:40:05 -0400

    Migration: AddCategoriaEstadoToBooks

---
+++ Biblion.Infrastructure/Migrations/20260305223848_AddCategoriaEstadoToBooks.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biblion.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoriaEstadoToBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Books",
+                newName: "estado");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Categoria",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Categoria",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "estado",
+                table: "Books",
+                newName: "Description");
+        }
+    }
+}
